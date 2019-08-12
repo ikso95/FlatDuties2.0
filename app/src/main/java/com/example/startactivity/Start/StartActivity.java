8100f83fde2eaf18a0c290734910e836cd5eaf31
Oskar <oskail@wp.pl>
2019-08-12 19:46:15 +0200

    remember user with sharedPrefferences

--- app/src/main/java/com/example/startactivity/Start/StartActivity.java
+++ app/src/main/java/com/example/startactivity/Start/StartActivity.java
@@ -1,13 +1,17 @@
 package com.example.startactivity.Start;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.example.startactivity.Common.Common;
 import com.example.startactivity.Main.MainActivity;
+import com.example.startactivity.Models.User;
 import com.example.startactivity.R;
 import com.example.startactivity.SignIn.SignInActivity;
 import com.example.startactivity.SignUp.SignUpActivity;
@@ -19,10 +23,17 @@ public class StartActivity extends AppCompatActivity {
     private Button next;
     private ViewPager viewPager;
 
+    private SharedPreferences preferences;
+    private int uzytkownikID,grupaID;
+    private String mail, nick;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+
+        isUserLoggedIn();
 
         //przejście do ekranu logowania
         sign_in = (Button) findViewById(R.id.sign_in_button_start);
@@ -56,6 +67,25 @@ public class StartActivity extends AppCompatActivity {
         viewPager = (ViewPager)findViewById(R.id.instructions_gallery_viewPager_start);
         ImageAdapter adapter = new ImageAdapter(this);
         viewPager.setAdapter(adapter);
+
+    }
+
+    private void isUserLoggedIn() {
+        preferences = getSharedPreferences("Login", MODE_PRIVATE);
+        uzytkownikID = preferences.getInt("UserID",0);
+        mail=preferences.getString("Email","");
+        nick=preferences.getString("Nick","");
+        grupaID=preferences.getInt("GroupID",0);
+
+        User u = new User(uzytkownikID,mail, nick, grupaID);
+        Common.currentUser=u;
+
+        if(uzytkownikID!=0) //jeżeli zostały odczytane poprawne dane, wartosc 0 jest domyślnie ustawiana, w bazie nie ma takiego rekordu
+        {
+            Intent intent = new Intent(StartActivity.this, MainActivity.class);
+            startActivity(intent);
+            Toast.makeText(StartActivity.this, "Signed in", Toast.LENGTH_SHORT).show();
+        }
 
     }
 }
